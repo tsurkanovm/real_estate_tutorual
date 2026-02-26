cfe1c5e56d1f8b6d9e92d613dda5d103cb78acd1
Tsurkanov Mykhailo <mikhail.tsurkanov@overdose.digital>
2026-02-26 14:50:31 +0200

    add "Real Estate Ads" module with custom views (form, list) and search

---
+++ addons/ud_estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Real Estate Ads',
+    'version': '1.0',
+    'category': 'Sales',
+    'description': """
+Module for practice Udemy and Tutorial course
+========================================
+    """,
+    'application': True,
+    'depends': [],
+    'data': [
+        'data/ir.model.access.csv',
+
+        'views/ud_estate_property_views.xml',
+        'views/ud_estate_property_menus.xml',
+    ],
+    'author': 'Mykhailo Tsurkanov',
+    'license': 'LGPL-3',
+}
---
+++ addons/ud_estate/models/ud_estate_property.py
@@ -0,0 +1,35 @@
+from dateutil.relativedelta import relativedelta
+
+from odoo import fields, models
+
+class Property(models.Model):
+    _name = 'ud_estate.property'
+    _description = 'Real Estate Property'
+
+    name = fields.Char(string='Title', nullable=False, required=True)
+    active = fields.Boolean(string='Active')
+    state = fields.Selection(
+        [('new', 'New'), ('received', 'Offer Received'), ('accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        string='Property State',
+        default='new'
+    )
+
+    description = fields.Text(string='Property Description')
+    postcode = fields.Char(string='Postcode')
+    price = fields.Float(string='Expected Price', nullable=False, required=True)
+
+    best_offer = fields.Float(string='Best Offer')
+    available_from = fields.Date(string='Available From',
+                                 default=fields.Date.today() + relativedelta(months=3), copy=False)
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
+    facades = fields.Integer(string='Facades', default=2)
+    living_area = fields.Integer(string='Living Area')
+    garden_area = fields.Integer(string='Garden Area')
+    total_area = fields.Integer(string='Total Area')
+    garden = fields.Boolean(string='Garden')
+    garage = fields.Boolean(string='Garage')
+
+    garden_orientation = fields.Selection(
+        [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        string='Garden Orientation'
+    )
