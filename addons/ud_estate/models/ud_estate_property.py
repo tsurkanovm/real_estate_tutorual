20ad7b6abd9ea078520cd84f2e81a6996eea28e0
Tsurkanov Mykhailo <mikhail.tsurkanov@overdose.digital>
2026-02-27 14:12:19 +0200

    add computed fields for property and offer models

--- addons/ud_estate/models/ud_estate_property.py
+++ addons/ud_estate/models/ud_estate_property.py
@@ -1,29 +1,27 @@
 from dateutil.relativedelta import relativedelta
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 class Property(models.Model):
     _name = 'ud_estate.property'
     _description = 'Real Estate Property'
 
-    name = fields.Char(string='Title', nullable=False, required=True)
+    name = fields.Char(string='Title', required=True)
     active = fields.Boolean(string='Active')
     state = fields.Selection(
         [('new', 'New'), ('received', 'Offer Received'), ('accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
         string='Property State',
         default='new'
     )
-    description = fields.Text(string='Property Description')
+
     postcode = fields.Char(string='Postcode')
-    price = fields.Float(string='Expected Price', nullable=False, required=True)
-    best_offer = fields.Float(string='Best Offer')
+    price = fields.Float(string='Expected Price', required=True)
     available_from = fields.Date(string='Available From',
                                  default=fields.Date.today() + relativedelta(months=3), copy=False)
     bedrooms = fields.Integer(string='Bedrooms', default=2)
     facades = fields.Integer(string='Facades', default=2)
     living_area = fields.Integer(string='Living Area')
     garden_area = fields.Integer(string='Garden Area')
-    total_area = fields.Integer(string='Total Area')
     garden = fields.Boolean(string='Garden')
     garage = fields.Boolean(string='Garage')
     garden_orientation = fields.Selection(
@@ -37,3 +35,32 @@ class Property(models.Model):
     user_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
     tag_ids = fields.Many2many('ud_estate.property.tag', string='Tags')
     offer_ids = fields.One2many('ud_estate.property.offer', 'property_id', string='Offers')
+
+
+    # computed fields
+    total_area = fields.Integer(string='Total Area', compute='_compute_total_area')
+    description = fields.Text(string='Property Description', compute='_compute_description', store=True)
+    best_offer = fields.Float(string='Best Offer', copy=False, compute='_compute_best_offer')
+
+    # -------------------------------------------------------------------------
+    # COMPUTE METHODS
+    # -------------------------------------------------------------------------
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for property in self:
+            property.total_area = property.living_area + property.garden_area
+
+    @api.depends('type_id.name', 'partner_id.name', 'total_area')
+    def _compute_description(self):
+        for property in self:
+            property.description \
+                = f"{property.type_id.name} for {property.partner_id.name} with total area {property.total_area} sqm"
+
+
+    @api.depends('offer_ids.price')
+    def _compute_best_offer(self):
+        for property in self:
+            if property.offer_ids:
+                property.best_offer = max(property.offer_ids.mapped('price'))
+            else:
+                property.best_offer = 0.0
--- addons/ud_estate/models/ud_estate_property_offer.py
+++ addons/ud_estate/models/ud_estate_property_offer.py
@@ -1,12 +1,35 @@
-
-from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+from odoo import api, fields, models
 
 class Property(models.Model):
     _name = 'ud_estate.property.offer'
     _description = 'Property Offers'
 
-    #name = fields.Char(string='Name',  required=True)
     price = fields.Float(string='Price')
     status = fields.Selection([('accepted', 'Accepted'), ('rejected', 'Rejected')], string='Status')
     property_id = fields.Many2one('ud_estate.property', string='Property', required=True)
     partner_id = fields.Many2one('res.partner', string='Bayer', required=True)
+    validity = fields.Integer(string='Validity', default=7)
+
+    #computed fields
+    date_deadline = fields.Datetime(string='Deadline',
+                                    compute='_compute_date_deadline',
+                                    inverse='_inverse_date_deadline')
+
+    # -------------------------------------------------------------------------
+    # COMPUTE AND INVERSE METHODS
+    # -------------------------------------------------------------------------
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for offer in self:
+            offer.date_deadline = fields.Datetime.now() + relativedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            deadline = offer.date_deadline
+            if isinstance(deadline, int):
+                deadline = datetime.fromtimestamp(deadline)
+
+            now = fields.Datetime.now()
+            offer.validity = (deadline - now).days
